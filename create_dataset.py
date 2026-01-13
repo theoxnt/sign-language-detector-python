386cdabfcc6bf9fef1c605264dbc0a8c8fd9b042
theoxnt <tnouet1306@gmail.com>
2026-01-13 08:53:42 +0100

    create_dataset modified - loop added to work with dataset from all group

--- create_dataset.py
+++ create_dataset.py
@@ -16,36 +16,37 @@ DATA_DIR = './data'
 
 data = []
 labels = []
-actual_label = 25
-for dir_ in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
-        data_aux = []
+number_of_classes = 2 
+for actual_label in range(number_of_classes):
+    for dir_ in os.listdir(DATA_DIR):
+        DATA_DIR_path = os.path.join(DATA_DIR, dir_)
+        print(DATA_DIR_path)
+        print(actual_label)
+        for img_path in os.listdir(os.path.join(DATA_DIR_path, str(actual_label))):
+            data_aux = []
 
-        x_ = []
-        y_ = []
+            x_ = []
+            y_ = []
+            img = cv2.imread(os.path.join(DATA_DIR_path, dir_, img_path))
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = hands.process(img_rgb)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
 
-        results = hands.process(img_rgb)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
+                        x_.append(x)
+                        y_.append(y)
 
-                    x_.append(x)
-                    y_.append(y)
-
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    data_aux.append(x - min(x_))
-                    data_aux.append(y - min(y_))
-
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
+                        data_aux.append(x - min(x_))
+                        data_aux.append(y - min(y_))
             data.append(data_aux)
             labels.append(actual_label)
-    actual_label -= 1
 
 f = open('data.pickle', 'wb')
 pickle.dump({'data': data, 'labels': labels}, f)
